25d486b0bc4d27825e420bedbac7bccbdcb0fa84
Alicia Gómez <146170489+PinkyAli@users.noreply.github.com>
2023-11-06 10:02:18 +0100

    Add files via upload

--- Tasca2/universidad_queries.sql
+++ Tasca2/universidad_queries.sql
@@ -15,3 +15,15 @@ SELECT d.nombre AS departamento, p.apellido1, p.apellido2, p.nombre FROM persona
 SELECT a.nombre AS asignatura, p.apellido1, p.apellido2, p.nombre FROM persona AS p LEFT JOIN asignatura AS a ON p.id = a.id_profesor WHERE p.tipo = "profesor" AND a.id IS NULL;
 SELECT a.nombre AS asignatura, p.apellido1, p.apellido2, p.nombre FROM persona AS p RIGHT JOIN asignatura AS a ON p.id = a.id_profesor WHERE p.id IS NULL;
 SELECT DISTINCT d.nombre AS departamento, a.nombre AS asignatura, a.curso FROM departamento AS d LEFT JOIN profesor AS pf ON d.id = pf.id_departamento LEFT JOIN persona AS p ON pf.id_profesor = p.id LEFT JOIN asignatura AS a ON p.id = a.id_profesor WHERE a.curso IS NULL;
+
+SELECT COUNT(nombre) AS numero_alumnos FROM persona WHERE tipo = "alumno";
+SELECT COUNT(nombre) AS numero_alumnos FROM persona WHERE tipo = "alumno" AND fecha_nacimiento LIKE "1999%";
+SELECT d.nombre AS departamento, COUNT(pf.id_profesor) AS numero_profesores FROM departamento AS d INNER JOIN profesor AS pf ON d.id = pf.id_departamento GROUP BY d.nombre ORDER BY numero_profesores DESC; 
+SELECT d.nombre AS departamento, COUNT(pf.id_profesor) AS numero_profesores FROM departamento AS d LEFT JOIN profesor AS pf ON d.id = pf.id_departamento GROUP BY d.nombre ORDER BY numero_profesores DESC;
+SELECT g.nombre AS grado, COUNT(a.id) AS numero_asignaturas FROM grado AS g LEFT JOIN asignatura AS a ON g.id = a.id_grado GROUP BY g.nombre ORDER BY numero_asignaturas DESC;
+SELECT g.nombre AS grado, COUNT(a.id) AS numero_asignaturas FROM grado AS g LEFT JOIN asignatura AS a ON g.id = a.id_grado GROUP BY g.nombre HAVING numero_asignaturas > 40;
+SELECT g.nombre AS grado, a.tipo, SUM(a.creditos) AS creditos FROM grado AS g LEFT JOIN asignatura AS a ON g.id = a.id_grado GROUP BY g.nombre, a.tipo ORDER BY g.nombre DESC;
+SELECT c.anyo_inicio, COUNT(am.id_alumno) AS numero_alumnos FROM curso_escolar AS c LEFT JOIN alumno_se_matricula_asignatura AS am ON c.id = am.id_curso_escolar GROUP BY c.anyo_inicio;
+SELECT p.id, p.nombre, p.apellido1, p.apellido2, COUNT(a.id) AS asignaturas FROM persona AS p LEFT JOIN asignatura AS a ON p.id = a.id_profesor WHERE p.tipo = "profesor" GROUP BY p.id ORDER BY asignaturas DESC;
+SELECT * FROM persona WHERE tipo = "alumno" ORDER BY fecha_nacimiento DESC LIMIT 1;
+SELECT p.nombre, p.apellido1, p.apellido2 FROM persona AS p INNER JOIN profesor AS pf ON p.id = pf.id_profesor LEFT JOIN asignatura AS a ON p.id = a.id_profesor WHERE a.id IS NULL;
